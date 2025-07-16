b922d71fd2b80a41d2493efd4c57bc8528178b1a
Vitalii Prokofiev <vitaliydps4@gmail.com>
2025-07-16 12:33:27 +0200

    initial file structure

---
+++ core/global-tts.py
@@ -0,0 +1,19 @@
+class GlobalTTS:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(GlobalTTS, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        if not hasattr(self, '_started'):
+            self._started = False
+            print("App initialized.")
+
+    def start(self):
+        if not self._started:
+            self._started = True
+            print("App started.")
+        else:
+            print("App already started.")
